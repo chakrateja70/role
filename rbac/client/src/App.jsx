887973bdfb56d860e52b458fbce6dc8dee23d21f
teja <tejapolamarasetty2004@gmail.com>
2024-06-18 16:34:57 +0530

    added Home Route

--- rbac/client/src/App.jsx
+++ rbac/client/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import Login from './Login/Login';
 import Register from './components/Register/Register';
 import Dashboard from './components/Dashboard/Dashboard';
 import Navbar from './components/Navbar/Navbar';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
 
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/dashboard" component={Dashboard} />
       </Switch>
---
+++ rbac/client/src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Login to access the site</h1>
+    </div>
+  )
+}
+
+export default Home
